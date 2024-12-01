4bd9490a921081c797713071b924d2a86722e445
V <vidyasikha1@gmail.com>
2024-12-02 00:11:27 +0530

    config updated

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Vidyashika', // Usually your GitHub org/user name.
   projectName: 'blog', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
